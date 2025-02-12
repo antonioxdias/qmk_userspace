cf8119f7c85997fe256bddbee0fb6929f0c4fd71
António Dias <a.fortuna.dias@gmail.comm>
2025-02-12 14:02:59 +0000

    Setup for master left

--- keyboards/bastardkb/charybdis/config.h
+++ keyboards/bastardkb/charybdis/config.h
@@ -1,4 +1,7 @@
-// #undef POINTING_DEVICE_RIGHT
-// #define POINTING_DEVICE_LEFT
+// #define POINTING_DEVICE_RIGHT
+// Pointing device is on the left split.
+#undef POINTING_DEVICE_RIGHT
+#define POINTING_DEVICE_LEFT
 
-// #define POINTING_DEVICE_INVERT_Y
+// #define POINTING_DEVICE_INVERT_X
+#define POINTING_DEVICE_INVERT_Y
