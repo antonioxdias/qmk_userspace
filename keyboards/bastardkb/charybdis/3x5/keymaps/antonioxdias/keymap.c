07c7c59670da5ea245d76fb0840dca8ddb021200
António Dias <a.fortuna.dias@gmail.comm>
2025-02-13 17:45:19 +0000

    Add extra thumb key

--- keyboards/bastardkb/charybdis/3x5/keymaps/antonioxdias/keymap.c
+++ keyboards/bastardkb/charybdis/3x5/keymaps/antonioxdias/keymap.c
@@ -62,31 +62,31 @@ static uint16_t auto_pointer_layer_timer = 0;
        KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P, \
        KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN, \
        KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M, KC_COMM,  KC_DOT, KC_MINS, \
-                               BSP_PTR, ENT_FUN, SPC_SYM, TAB_NAV, KC_MPLY
+                               BSP_PTR, ENT_FUN, SPC_SYM, TAB_NAV, KC_MPLY, XXXXXXX
 
 #define LAYOUT_LAYER_NUMNAV                                                                   \
     XXXXXXX,    KC_7,    KC_8,    KC_9, XXXXXXX, KC_HOME, KC_PGDN, KC_PGUP,  KC_END, XXXXXXX, \
     XXXXXXX,    KC_4,    KC_5,    KC_6,    KC_0, KC_LEFT, KC_DOWN,   KC_UP,KC_RIGHT, XXXXXXX, \
     XXXXXXX,    KC_1,    KC_2,    KC_3, XXXXXXX, XXXXXXX, XXXXXXX, KC_COMM,  KC_DOT, XXXXXXX, \
-                               KC_BSPC,  KC_ENT,  KC_SPC, KC_TRNS,    KC_0
+                               KC_BSPC,  KC_ENT,  KC_SPC, KC_TRNS,    KC_0, XXXXXXX
 
 #define LAYOUT_LAYER_SYMBOLS                                                                  \
     KC_EXLM,   KC_AT, KC_HASH,  KC_DLR, KC_PERC, KC_AMPR, KC_SLSH, KC_QUOT, KC_DQUO, KC_ASTR, \
     KC_PIPE,   KC_NO,   KC_NO, KC_LPRN, KC_LCBR, KC_RCBR, KC_RPRN, KC_QUES, KC_PEQL, KC_PLUS, \
     KC_BSLS, XXXXXXX, XXXXXXX,   KC_LT, KC_LBRC, KC_RBRC,   KC_GT, KC_TILD,  KC_GRV, KC_CIRC, \
-                               KC_BSPC,  KC_ENT, KC_TRNS,  KC_TAB, XXXXXXX
+                               KC_BSPC,  KC_ENT, KC_TRNS,  KC_TAB, XXXXXXX, XXXXXXX
 
 #define LAYOUT_LAYER_FNMEDIA                                                                  \
     XXXXXXX,   KC_F7,   KC_F8,   KC_F9,  KC_F10, KC_MPRV, KC_VOLD, KC_VOLU, KC_MNXT, KC_MPLY, \
     KC_CAPS,   KC_F4,   KC_F5,   KC_F6,  KC_F11, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, \
     KC_PSCR,   KC_F1,   KC_F2,   KC_F3,  KC_F12, XXXXXXX, KC_BRMD, KC_BRMU, XXXXXXX, XXXXXXX, \
-                                KC_DEL, KC_TRNS,  KC_SPC,  KC_ESC, KC_MPLY
+                                KC_DEL, KC_TRNS,  KC_SPC,  KC_ESC, KC_MPLY, XXXXXXX
 
 #define LAYOUT_LAYER_POINTER                                                                  \
     XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_HOME, KC_PGDN, KC_PGUP,  KC_END, XXXXXXX, \
     XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_WH_L, KC_WH_D, KC_WH_U, KC_WH_R, XXXXXXX, \
     XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_BTN4, KC_BTN5, XXXXXXX, XXXXXXX, \
-                               KC_TRNS, KC_BTN3, KC_BTN1, KC_BTN2, XXXXXXX
+                               KC_TRNS, KC_BTN3, KC_BTN1, KC_BTN2, XXXXXXX, XXXXXXX
 
 /**
  * \brief Add Home Row mod to a layout.
